d0b48c967e62928b2fafd399f2d926ddd7766487
Ethan <c0043883@hallam.shu.ac.uk>
2023-10-11 15:30:15 +0100

    Adds gui to client, supports connection

--- Client.py
+++ Client.py
@@ -1,15 +1,48 @@
 import socket
+from tkinter import *
+
+
+def show_data():
+    global serverIP
+    global rawPort
+    serverIP = servIP.get()
+    rawPort = port.get()
+    print( 'Connecting to %s on port %s' % (servIP.get(), port.get()) )
+    print(serverIP)
+    print(rawPort)
+    win.quit()
+
+
+win = Tk()
+win.title("B33Hive: Connect to a Server")
+win.geometry("640x480")
+Label( win, text='Server IP' ).grid( row=0 )
+Label( win, text='Port' ).grid( row=1 )
+
+servIP = Entry( win )
+port = Entry( win )
+
+servIP.grid( row=0, column=1 )
+port.grid( row=1, column=1 )
+
+Button( win, text='Exit', command=win.quit ).grid( row=3, column=0, sticky=W, pady=4 )
+Button( win, text='Connect', command=show_data ).grid( row=3, column=1, sticky=W, pady=4 )
+
+mainloop()
+
 
 # server's IP address
 # if the server is not on this machine,
 # put the private (network) IP address (e.g 192.168.1.2)
-SERVER_HOST = input("Enter IP Address or for Local Chats: 127.0.0.1: ")
-SERVER_PORT = 5003 # server's port
+SERVER_HOST = serverIP #("Enter IP Address or for Local Chats: 127.0.0.1: ")
+SERVER_PORT = rawPort # server's port
 separator_token = "<SEP>" # we will use this to separate the client name & message
 
 # initialize TCP socket
 s = socket.socket()
 print(f"[*] Connecting to {SERVER_HOST}:{SERVER_PORT}...")
+SERVER_HOST = str(SERVER_HOST)
+SERVER_PORT = int(SERVER_PORT)
 # connect to the server
 s.connect((SERVER_HOST, SERVER_PORT)) #### From this point on, we're talking to the server
 print("[+] Connected.")
--- Server.py
+++ Server.py
@@ -19,42 +19,22 @@ s.bind((SERVER_HOST, SERVER_PORT))
 s.listen(5)
 print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
 
-def listen_for_client(cs): ##This is message handling code left over from the chat client, I have kept it as it may be able to be repurposed
-    """
-    This function keep listening for a message from cs socket
-    Whenever a message is received, broadcast it to all other connected clients
-    """
-    while True:
-        try:
-            # keep listening for a message from cs socket
-            msg = cs.recv(1024)
-        except Exception as e:
-            # client no longer connected
-            # remove it from the set
-            print(f"[!] Error: {e}")
-            client_sockets.remove(cs)
-        # else:
-        #     # if we received a message, replace the <SEP>
-        #     # token with ": " for nice printing
-        #     msg = msg.replace(separator_token, ": ")
-        # iterate over all connected sockets
-        for client_socket in client_sockets:
-            # and send the message
-            client_socket.send(msg)
+#
 
 
 while True:
     # we keep listening for new connections all the time
     client_socket, client_address = s.accept()
-    print(f"[+] {client_address} connected.")
+    #print(f"[+] {client_address} connected.") ##we have no fucking idea why this prints an additional number (eg: 51xxx)
+    #print(client_socket)
     # add the new connected client to connected sockets
     client_sockets.add(client_socket)
     # start a new thread that listens for each client's messages
-    t = Thread(target=listen_for_client, args=(client_socket,))
+    #t = Thread(target=listen_for_client, args=(client_socket,))
     # make the thread daemon, so it ends whenever the main thread ends
-    t.daemon = True
+    #t.daemon = True
     # start the thread
-    t.start()
+    #t.start()
 
 
 
